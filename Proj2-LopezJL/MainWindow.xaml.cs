97c6dccabf3cc51939633b00ecc38eeaca0cfbc8
contactjorge <contactjorge@thejorgelopez.com>
2018-04-21 19:27:24 -0500

    Update

--- Proj2-LopezJL/MainWindow.xaml.cs
+++ Proj2-LopezJL/MainWindow.xaml.cs
@@ -25,23 +25,36 @@ namespace Proj2_LopezJL
             InitializeComponent();
         }
 
-
         private void pincipalAmount_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
 
-        private void radioButton15_Checked(object sender, RoutedEventArgs e)
+        private void rdoButton15_Clicked(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void radioButton30_Checked(object sender, RoutedEventArgs e)
+        private void rdoButton15_Checked(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void radioButtonOther_Checked(object sender, RoutedEventArgs e)
+        private void rdoButton30_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void rdoButton30_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rdoButtonOther_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rdoButtonOther_Click(object sender, RoutedEventArgs e)
         {
 
         }
@@ -60,5 +73,7 @@ namespace Proj2_LopezJL
         {
 
         }
+
+
     }
 }
